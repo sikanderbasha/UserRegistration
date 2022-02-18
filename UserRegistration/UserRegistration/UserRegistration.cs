9c8279b2960a830b80fee75dd35b854072c454a8
Sikander <sikku.rk405@gmail.com>
2022-02-18 17:47:35 +0530

    [Sikander] Added firstname program using regex

---
+++ UserRegistration/UserRegistration/UserRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class UserRegistration
+    {
+          public void Firstname()
+          {
+            Regex firstname = new Regex("^[A-Z]{1}[a-z]{3,15}$");
+            Console.WriteLine("Enter first Name :");
+            string first = Console.ReadLine();
+            if (firstname.IsMatch(first))
+            {
+                Console.WriteLine("First Name : " + first);
+            }
+            else
+            {
+                Console.WriteLine("Enter name in valid format");
+            }
+        }
+    }
+}
